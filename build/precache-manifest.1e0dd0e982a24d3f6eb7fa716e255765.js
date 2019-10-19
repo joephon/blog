d5f9557daf000c9827be921a3666057aa876e303
joephon <306519779@qq.com>
2019-10-20 04:04:20 +0800

    ok

---
+++ build/precache-manifest.1e0dd0e982a24d3f6eb7fa716e255765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6026dd83e73112a3a79613f274652147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3519335844a70547f53c",
+    "url": "/static/css/main.3606ed71.chunk.css"
+  },
+  {
+    "revision": "0aae08a62b1fd27a536a",
+    "url": "/static/js/2.054b8be4.chunk.js"
+  },
+  {
+    "revision": "3519335844a70547f53c",
+    "url": "/static/js/main.cdd7ec93.chunk.js"
+  },
+  {
+    "revision": "6dbe56ce70542469ff0d",
+    "url": "/static/js/runtime-main.04784543.js"
+  }
+]);
